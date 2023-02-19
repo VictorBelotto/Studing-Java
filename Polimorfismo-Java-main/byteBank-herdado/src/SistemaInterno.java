bca2e72ec33d43143a1639a44d58ee3e64b06306
VictorBelotto <victortucunduva.vb@outlook.com>
2023-02-18 21:38:50 -0300

    Finalizado curso de polimorfismo

--- Polimorfismo-Java-main/byteBank-herdado/src/SistemaInterno.java
+++ Polimorfismo-Java-main/byteBank-herdado/src/SistemaInterno.java
@@ -6,10 +6,10 @@ public class SistemaInterno {
 	public void autentica(Autenticavel fa) {
 		boolean autenticou = fa.autentica(this.senha);
 		if(autenticou) {
-			System.out.println("Senha correta");
+			System.out.println("Senha correta para: " + fa);
 			return;
 		}
-		System.out.println("Senha incorreta");
+		System.out.println("Senha incorreta para: " +  fa);
 	}
 	
 }
--- Polimorfismo-Java-main/byteBank-herdado/src/TesteSistema.java
+++ Polimorfismo-Java-main/byteBank-herdado/src/TesteSistema.java
@@ -8,12 +8,18 @@ public class TesteSistema {
 		Administrador adm = new Administrador();
 		adm.setSenha(2222);
 		
+		Cliente cliente = new Cliente();
+		cliente.setSenha(2222);
+		
 		SistemaInterno gSistema = new SistemaInterno();
 		gSistema.autentica(g);
 		
 		SistemaInterno admSistema = new SistemaInterno();
 		admSistema.autentica(adm);
-			
+		
+		SistemaInterno clienteSistema = new SistemaInterno();
+		clienteSistema.autentica(cliente);
+		
 	}
 	
 }
